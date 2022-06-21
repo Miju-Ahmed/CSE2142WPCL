ff3404df3f1c51374e8643cecf5acc4b2fdf8e52
Miju <miju.ch7@gmail.com>
2022-06-21 22:15:22 +0000

    Adding the Constant class to remove the double quotation elements from the main function

---
+++ Constant.java
@@ -0,0 +1,17 @@
+public class Constant {
+    public static final String studentList ="StudentList.txt";
+    public static final String loadingData = "Loading Data...";
+    public static final String dataLoaded = "Data Loaded";     
+    public static final String dateShow = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String comma = ", ";
+    public static final String weFound = "We found it!";
+    public static final String wordFound = " word(s) found ";
+    public static final String showAll = "a";
+    public static final String random = "r";
+    public static final String plus = "+";
+    public static final String wordCount = "c";
+    public static final String subString = "?";
+    public static final String notFound = "Input argument is not matched.";
+    public static final String update = "\nList last updated on ";
+    public static final char space = ' ';
+}
